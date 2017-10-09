ce6440427aa77794fa2dd7b8f44e3d7dbbd8e214
seolanc <seolanc@gmail.com>
2017-10-09 23:32:39 +0300

    11

---
+++ javascript.js
@@ -0,0 +1,11 @@
+ var button = document.querySelector(".write-us");
+ var modal = document.querySelector(".modal");
+ var mouseover = modal.querySelector(".close");
+          
+ button.addEventListener("click", function () {
+     modal.classList.toggle("open");
+
+          });
+  
+        
+          
